8c717ae3211ef9628552bb6261034915977bf9d6
david3328 <davidjsupoo@gmail.com>
2019-07-20 13:32:20 -0500

    Hackaton BBVA

--- src/app.js
+++ src/app.js
@@ -3,8 +3,13 @@ const app = express();
 const cors = require('cors');
 
 app.use(cors());
+
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
+
+
+
+app.use(express.static(__dirname + '/public'));
 
 //Rutas 
 const rutas = require('./routes');
